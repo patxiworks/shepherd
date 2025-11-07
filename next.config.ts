9d1acef03dd942fd4689bb9502b2a06979ef0ccc
Patrick Enaholo <patxiworks@gmail.com>
2025-11-07 08:36:28 +0000

    I still have @ducanh2912/next-pwa in package.json. Do I need it? Can it

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type {NextConfig} from 'next';
-import withPWA from '@ducanh2912/next-pwa';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -27,11 +26,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-const pwaConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-export default pwaConfig(nextConfig);
+export default nextConfig;
